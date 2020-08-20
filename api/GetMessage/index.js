1a51f43ca092399b38b9606fff00e5524841f416
Amit <amit@cakewalk.in>
2020-08-21 02:40:23 +0530

    express

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,7 +1,10 @@
-module.exports = async function (context, req) {
-  context.res = {
-    body: {
-      text: "Hello from the API",
-    },
-  };
-};
+const createHandler = require("azure-function-express").createHandler;
+const express = require("express");
+const app = express();
+app.get("/:foo/:bar", (req, res) => {
+  res.json({
+    foo: req.params.foo,
+    bar: req.params.bar,
+  });
+});
+module.exports = createHandler(app);
